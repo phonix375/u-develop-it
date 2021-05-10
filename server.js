d27f7b03e69bbaf0ec904bec7fd38dc6bdd3abc7
Alexy Kotliar <kotliar.alex@gmail.com>
2021-05-09 23:35:00 -0400

    created the querys for the database

--- server.js
+++ server.js
@@ -19,12 +19,42 @@ const db = mysql.createConnection(
     console.log('connect to the database')
 
 )
+// show all candidates
+// db.query(`SELECT * FROM candidates`, (err, rows) => {
+//     console.log(rows);
+//   });
 
-db.query(`SELECT * FROM candidates`, (err, rows) => {
-    console.log(rows);
-  });
+//select one candidate by ID
+// db.query(`SELECT * FROM candidates WHERE id = 20`, (err, row) =>{
+//     if(err){
+//         console.log(err);
+//     }else{
+//         console.log(row);
+//     }
+// })
 
 
+// delete
+// db.query(`DELETE FROM candidates WHERE id = ?`,1,(err,result)=>{
+//     if(err){
+//         console.log(err);
+//     }
+//     else{
+//         console.log(result);
+//     }
+// })
+// //create a candidate
+
+// const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected) VALUES (?,?,?,?)`;
+// const params = [1,'Ronald', 'Firbank', 1];
+
+// db.query(sql, params, (err,result) => {
+//     if(err){
+//         console.log(err);
+//     }else{
+//         console.log(result);
+//     }
+// });
 //default response to all unfound pages
 app.use((req, res) => {
     res.status(404).end();
