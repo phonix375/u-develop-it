f755a439e5f588db140816662641c26c9019e5af
Alexy Kotliar <kotliar.alex@gmail.com>
2021-05-09 23:16:29 -0400

    connected express server to MySQL

--- server.js
+++ server.js
@@ -1,13 +1,31 @@
 const express = require('express');
+const mysql = require('mysql2');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
 
+//express midleware
 app.use(express.urlencoded({ extended: false}));
 app.use(express.json());
 
+//connect to the database
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        user: 'root',
+        password: 'Zxasqw!@12',
+        database: 'election'
+    },
+    console.log('connect to the database')
+
+)
+
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+    console.log(rows);
+  });
 
 
+//default response to all unfound pages
 app.use((req, res) => {
     res.status(404).end();
 })
